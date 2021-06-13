e654930753e3d601b486f523e4bbc2780de3dbb3
Abraao L <abraao.lacerda@buser.com.br>
2021-06-13 20:57:05 -0300

    Add board to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import { useCallback, useEffect, useState } from 'react'
+
+const Row = ({ children }) => {
+  return <div className="row">
+    {children}
+  </div>
+}
+
+const Cell = ({ x, y, isSelected, setSelected }) => {
+  const classNames = `cell ${isSelected && 'selected'}`
+  return <div
+    className={classNames}
+    onClick={() => setSelected(x, y)}
+  >
+    <p>[{x}, {y}]</p>
+  </div>
+}
 
 function App() {
+  const n_rows = 20
+  const n_cell = 20
+  const [selected, setSelected] = useState({})
+  const [tool, setTool] = useState('scanline')
+
+
+
+  const onSelect = (x, y) => {
+    for (let j = x; j < n_rows; j++) {
+      for (let i = y; i < n_cell; i++) {
+        setTimeout(()=>setSelected(selected => ({...selected, [j+100*i]:true})), 40*(i-y))
+      }
+    }
+  }
+
+  const renderRow = useCallback(() => {
+    console.log("RENDER")
+    let arr = []
+    for (let x = 0; x < n_rows; x++) {
+      let row = []
+      for (let y = 0; y < n_cell; y++) {
+        row.push(<Cell
+          x={x}
+          y={y}
+          isSelected={selected[x + y * 100]}
+          setSelected={onSelect}
+        />)
+      }
+      arr.push(<Row>
+        {row}
+      </Row>)
+    }
+    return arr
+  }, [selected])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        
+      </Row> 
+      {renderRow()}
     </div>
   );
 }
